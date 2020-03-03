7f946a1dad3e1e78e1c968365ed04d7780d25241
Jetson <jetson@lionfihsMQP.com>
2020-03-03 18:29:45 -0500

    work on tensorRT conversion

--- tensor_RT/conversion_test.py
+++ tensor_RT/conversion_test.py
@@ -1,5 +1,5 @@
 from tensorflow.python.compiler.tensorrt import trt_convert as trt
 # Convert a saved model
-converter = trt.TrtGraphConverter(input_saved_model_dir='../mobilenet_output_inference_graph')
+converter = trt.TrtGraphConverterV2(input_saved_model_dir='../models/mobilenet_output_inference_graph/saved_model')
 graph_def = converter.convert()
 converter.save('mobilenetTRT/')
